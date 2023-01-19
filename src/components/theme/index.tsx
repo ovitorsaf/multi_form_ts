e66cae06f6ac86c7069edf503eec565dfd53b44a
ovitorsaf <vitoraffemp@gmail.com>
2023-01-19 02:07:57 -0300

    feat: sidebar completa

--- src/components/theme/index.tsx
+++ src/components/theme/index.tsx
@@ -1,12 +1,17 @@
 import { ReactNode } from 'react';
 import * as C from './styles';
 import { Header } from '../header';
+import { SideBarItem } from '../sidebaritem';
+import { useForm } from '../../contexts/FormContext';
 
 type Props = {
     children: ReactNode,
 }
 
 export const Theme = ({children}: Props) => {
+    
+    const { state } = useForm();
+    
     return(
         <C.Container>
             <C.Area>
@@ -14,7 +19,29 @@ export const Theme = ({children}: Props) => {
 
                 <C.Steps>
                     <C.Sidebar>
-                        ...
+                        <SideBarItem 
+                            title="Pessoal"
+                            description="Se identifique"
+                            icon="profile"
+                            path="/"
+                            active={state.currentStep === 1}
+                        />
+
+                        <SideBarItem 
+                            title="Profissional"
+                            description="Seu nível"
+                            icon="book"
+                            path="/step2"
+                            active={state.currentStep === 2}
+                        />
+
+                        <SideBarItem 
+                            title="Contatos"
+                            description="Como te achar"
+                            icon="mail"
+                            path="/step3"
+                            active={state.currentStep === 3}
+                        />    
                     </C.Sidebar>
                     <C.Page>
                         {children}
